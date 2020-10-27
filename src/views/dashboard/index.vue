33d0d9306ce1f2f56f15bfa22c58316fc4d888c6
Fire-zy <1604176679@qq.com>
2020-10-27 19:25:37 +0800

    first commit

---
+++ src/views/dashboard/index.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="dashboard-container">
+    <el-tabs style="margin: 30px 0px" type="border-card">
+      <!-- 添加的按钮 -->
+      <div class="header">
+        <div class="header-left">
+          <svg-icon icon-class="list" />
+          View List
+        </div>
+        <div class="component-item">
+          <el-button
+            @click="toSqlEdit"
+            v-waves
+            type="primary"
+            icon="el-icon-plus"
+            circle
+          ></el-button>
+          <!-- 打开添sql编辑器 -->
+          
+        </div>
+      </div>
+      <!-- sql表 -->
+      <sql-table ref="child"></sql-table>
+      <!-- 分页 -->
+      <el-pagination background layout="prev, pager, next" :total="1000">
+      </el-pagination>
+    </el-tabs>
+  </div>
+</template>
+
+<script>
+import waves from "@/directive/waves/index.js"; // 水波纹指令
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'Dashboard',
+  directives: {
+    waves,
+  },
+  computed: {
+    ...mapGetters([
+      'name'
+    ])
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.dashboard-container {
+  margin: 30px;
+}
+
+.ceshi{
+  margin-left: 120px;
+}
+
+.header {
+  padding-left: 10px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 15px;
+}
+.icon-class {
+  color: black;
+}
+
+.el-pagination {
+  margin-top: 20px;
+  margin-left: -10px;
+}
+</style>
